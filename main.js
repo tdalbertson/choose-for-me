45076b54be2020ca152678ccf172550ce3f5e50e
Tim Albertson <68976799+tdalbertson@users.noreply.github.com>
2024-02-02 17:09:32 -0800

    Add height & width setter for wrapper element
    - In order for the yellow wrapper element to accurately wrap the
    content-wrapper, the height and width need to be adjusted after page
    load. Otherwise, it will appear off-center.

--- main.js
+++ main.js
@@ -1,10 +1,22 @@
 const submitButton = document.getElementById("submit-button");
 let choice1, choice2, inputs;
 
-
 submitButton.addEventListener("click", () => {
     inputs = document.querySelectorAll("[data-input-id]");
     choice1 = inputs[0].value;
     choice2 = inputs[1].value;
     console.log(`choice1 = ${choice1}\nchoice2 = ${choice2}`);
 });
+
+// For setting yellow "border" of main content container
+let contentWrapper = document.querySelector(".content-wrapper");
+let wrapperHeight = contentWrapper.offsetHeight;
+let wrapperWidth = contentWrapper.offsetWidth;
+
+function setWrapperHeightWidth(height, width) {
+    let wrapper = document.querySelector(".wrapper");
+    wrapper.style.height = height + "px";
+    wrapper.style.width = width + "px";
+}
+
+setWrapperHeightWidth(wrapperHeight, wrapperWidth);
